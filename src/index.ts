61fd7b330f45d6ef7d7e0d9c26b43fb0b7e21869
Yustaf Pramsistya <37360793+taftphir@users.noreply.github.com>
2025-11-24 21:44:18 +0700

    test 1

--- src/index.ts
+++ src/index.ts
@@ -17,13 +17,14 @@ app.get("/", (req, res) => {
 });
 
 // router kamu
-app.use('/', authRoutes);
-app.use('/', aktifitasRoutes);
-app.use('/', omzetRoutes);
-app.use('/', gpsRoutes);
+// app.use('/', authRoutes);
+// app.use('/', aktifitasRoutes);
+// app.use('/', omzetRoutes);
+// app.use('/', gpsRoutes);
 
 // debug: pastikan routes ter-mount saat server start
-console.log('Routes mounted: / (authRoutes, aktifitasRoutes, omzetRoutes, gpsRoutes)');
+// console.log('Routes mounted: / (authRoutes, aktifitasRoutes, omzetRoutes, gpsRoutes)');
+console.log('Routes mounted: / basic');
 
 app.listen(port, () => {
   console.log(`API listening on ${port}`);
