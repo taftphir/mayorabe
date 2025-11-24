aa1095bb81b2f95feaf95a7bb49218691e18409f
Yustaf Pramsistya <37360793+taftphir@users.noreply.github.com>
2025-11-25 05:22:11 +0700

    ideal

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-// import * as dotenv from 'dotenv';
-//dotenv.config(); // <-- pastikan ini dijalankan sebelum import apapun yang memakai process.env
+import * as dotenv from 'dotenv';
+dotenv.config(); // <-- pastikan ini dijalankan sebelum import apapun yang memakai process.env
 
 import express from 'express';
-// import authRoutes from './routes/auth';
+import authRoutes from './routes/auth';
 import aktifitasRoutes from './routes/aktifitas';
-// import omzetRoutes from './routes/omzet';
-// import gpsRoutes from './routes/gps';
+import omzetRoutes from './routes/omzet';
+import gpsRoutes from './routes/gps';
 
 const app = express();
 app.use(express.json());
@@ -26,10 +26,10 @@ process.on("unhandledRejection", err => {
 });
 
 // router kamu
-// app.use('/', authRoutes);
+app.use('/', authRoutes);
 app.use('/', aktifitasRoutes);
-// app.use('/', omzetRoutes);
-// app.use('/', gpsRoutes);
+app.use('/', omzetRoutes);
+app.use('/', gpsRoutes);
 
 // debug: pastikan routes ter-mount saat server start
 console.log('Routes mounted: / (authRoutes, aktifitasRoutes, omzetRoutes, gpsRoutes)');
